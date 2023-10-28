f97ab44cb0734629dc3ccfef9c473c6366043c97
Sharanu <sharanugj22@gmail.com>
2023-10-28 11:31:18 +0530

    commited

--- Resources/UserKeywords.py
+++ Resources/UserKeywords.py
@@ -24,7 +24,89 @@ def get_matching_name(string, list_names):
             return name
 
 
+@keyword("Get Word Count")
+def get_word_count(string, list_names):
+    cnt = 0
+    for name in list_names:
+        if string in name:
+            cnt = cnt + 1
+    return cnt
+
+    # --------------------UserKeyowrds.py
+
+
+@keyword("Get Matching String")
+def get_matching_name(string, list_names):
+    for name in list_names:
+        if string in name:
+            return name
+
+
+# list_names = ['Abhi', 'Dixit', 'Dixit10234', 'Pooja', 'Ravi', 'David', 'Rahul']
+# get_matching_name('Dixit', list_names)
+
+@keyword("Get Word Count")
+def get_word_count(string, list_names):
+    cnt = 0
+    for name in list_names:
+        if string in name:
+            cnt = cnt + 1
+    return cnt
+
+
+# ---------------------------------------------------
+
+
+@keyword("Get Matching String")
+def get_matching_name(string, list_names):
+    for name in list_names:
+        if string in name:
+            return name
+
+
+# list_names = ['Abhi', 'Dixit', 'Dixit10234', 'Pooja', 'Ravi', 'David', 'Rahul']
+# get_matching_name('Dixit', list_names)
+
+@keyword("Get Word Count")
+def get_word_count(string, list_names):
+    cnt = 0
+    for name in list_names:
+        if string in name:
+            cnt = cnt + 1
+    return cnt
+
+
+# Robot
+# Test
+# case -
+#
+# validate and check
+# the
+# box
+# IF    ${Element_Count} > 0
+#
+# FOR    ${index}
+# IN
+# RANGE
+# 1    ${Element_Count + 1}
+# ${xpath} = xpath
+# creator       ${index}
+# Select
+# Checkbox     ${xpath}
+# END
+# END
+# Sleep
+# 10
+# s
+
+
 @keyword("xpath creator")
+def crete_xpath(index):
+    xpath = "(//*[text()='Salmon Island'])[" + str(index) + "]//..//td//input"
+    return xpath
+
+
+@keyword("xpath creators")
 def crete_xpath(uservalue, index):
     xpath = "(//*[text()='" + uservalue + "'])[" + str(index) + "]//..//td//input"
     return xpath
